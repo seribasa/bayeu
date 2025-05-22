35395793ac29c3a3757be2c5fb3e65d4a1c67d29
Rizky Faturriza <rfaturriza@gmail.com>
2025-05-22 21:20:56 +0700

    Refactor Supabase client initialization and update BaseResponse type syntax

--- supabase/functions/_shared/paymentSupabase.ts
+++ supabase/functions/_shared/paymentSupabase.ts
@@ -1,17 +1,26 @@
 import { createClient } from "jsr:@supabase/supabase-js@2";
 
 const PAYMENT_SUPABASE_URL = Deno.env.get("SUPABASE_URL");
-const PAYMENT_SUPABASE_SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY");
+const PAYMENT_SUPABASE_SERVICE_ROLE_KEY = Deno.env.get(
+  "SUPABASE_SERVICE_ROLE_KEY",
+);
 const PAYMENT_SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY");
 
 const paymentSupabaseAdmin = createClient(
   PAYMENT_SUPABASE_URL ?? "",
-  PAYMENT_SUPABASE_SERVICE_ROLE_KEY ?? ""
+  PAYMENT_SUPABASE_SERVICE_ROLE_KEY ?? "",
 );
 
-const paymentSupabaseClient = (header: any) =>
-  createClient(PAYMENT_SUPABASE_URL ?? "", PAYMENT_SUPABASE_ANON_KEY ?? "", {
-    global: { headers: header },
-  });
+const paymentSupabaseClient = (
+  { authorization }: { authorization: string },
+) => {
+  return createClient(
+    PAYMENT_SUPABASE_URL ?? "",
+    PAYMENT_SUPABASE_ANON_KEY ?? "",
+    {
+      global: { headers: { Authorization: authorization } },
+    },
+  );
+};
 
 export { paymentSupabaseAdmin, paymentSupabaseClient };
