52d8db30e66d0803ec42e961f6a99953dc5a01aa
Rizky Faturriza <rfaturriza@gmail.com>
2026-07-26 13:20:21 +0700

    feat(migrations): add NOTIFY command to reload schema after altering orders table

--- supabase/migrations/20260628165000_add_gateway_and_metadata_to_orders.sql
+++ supabase/migrations/20260628165000_add_gateway_and_metadata_to_orders.sql
@@ -3,6 +3,8 @@ ALTER TABLE orders
 ADD COLUMN gateway VARCHAR(255),
 ADD COLUMN metadata JSONB DEFAULT '{}'::jsonb;
 
+NOTIFY pgrst, 'reload schema';
+
 -- migrate:down
 ALTER TABLE orders
 DROP COLUMN gateway, metadata;
